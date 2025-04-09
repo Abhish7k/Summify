9cff0ce9f472b5183593c5e53131974f12687f9d
Abhish7k <abhishekbhosale.dev@gmail.com>
2025-04-09 10:51:38 +0530

    feat: final
    final changes to finish the changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ const App = () => {
   return (
     <main>
       <div class="relative w-full h-[100vh] bg-white">
-        <div class="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
-
         <div className="app">
           <div className="gradient" />
           <Hero />
